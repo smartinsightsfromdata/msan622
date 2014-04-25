9923b1dc50a9ef2465b90763edef5c3985505eec
justmytwospence <spencer.g.boucher@gmail.com>
2014-04-24 19:28:15 -0700

    Incorporate seatbelt data, modify time slider, revamp theme

--- homework5/server.R
+++ homework5/server.R
@@ -1,26 +1,13 @@
 shinyServer(function(input, output) {
   
-  output$start <- renderUI({
-    
-    if (is.null(input$num)) { return() }
-    
-    sliderInput("start", "Starting Point:",
-                min = 1974, max = 1980 - (input$num / 12) - (1/12),
-                value = 1974, step = 1 / 12,
-                round = FALSE, ticks = TRUE,
-                format = "####.##",
-                animate = animationOptions(
-                  interval = 800, loop = TRUE))
-  })
-  
   output$mainPlot <- renderPlot({
-    if (is.null(input$start)) { return() }
-    print(plotArea(input$start, input$num))
-  })
+    if (is.null(input$slider)) { return() }
+    print(plotArea(input$slider))
+  }, bg = 'transparent')
   
   output$overviewPlot <- renderPlot({
-    if (is.null(input$start)) { return() }
-    print(plotOverview(input$start, input$num))
-  })
+    if (is.null(input$slider)) { return() }
+    print(plotOverview(input$slider))
+  }, bg = 'transparent')
     
 })
--- homework5/ui.R
+++ homework5/ui.R
@@ -1,15 +1,15 @@
-shinyUI(pageWithSidebar(
+shinyUI(fluidPage(theme = 'bootstrap.min.css',
   headerPanel('Seatbelt deaths'),
   
   sidebarPanel(width = 3,
-               sliderInput('num', 'Months:', 
-                           min = 4, max = 24,
-                           value = 12, step = 1),
-               uiOutput('start')
+               sliderInput('slider', 'Time window',
+                           min = 1969, max = 1985,
+                           value = c(1969, 1970), step = 1/12,
+                           round = FALSE, ticks = TRUE,
+                           format = "####.##")
   ),
   
   mainPanel(width = 9,
-            textOutput('test'),
             plotOutput(outputId = "mainPlot", 
                        width = "100%", height = "400px"),
             plotOutput(outputId = "overviewPlot",
